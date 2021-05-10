0fc8bf9e73a25010d4e574ccd18d6789f5213df9
Arun Kudiyal <arunkudiyal@icloud.com>
2021-05-10 12:07:35 +0530

    JS Day - 1

---
+++ JavaScript/main.js
@@ -0,0 +1,67 @@
+// BASICS OF JS
+
+// Outputs
+console.log('Hello...')
+// alert('Hi...') // This is halt the markup 
+// console.warn('This is a warning message!')
+// console.error('This is an error!')
+
+// ES6 Scoping
+// let, var, const | let has a local scope | var has a global scope | const has a global scope
+// let a
+// a = 'Arun'
+// console.log(a)
+// a = undefined
+// console.log(a)
+
+// var percent;
+// percent = 86.890
+// console.log(percent);
+// percent = 20
+// console.log(percent);
+
+// const isOkay = true
+// // Cannot chnage 
+// // isOkay = false
+// console.log(isOkay);
+
+// PRIMITIVE DATA TYPES IN JS
+
+// number
+// const num = 10
+// console.log(typeof num);
+
+// // string
+// const name = 'Shreashi'
+// console.log(typeof name);
+
+// // boolean
+// const isCool = false
+// console.log(typeof isCool);
+
+// // undefined
+// const val1 = undefined
+// console.log(typeof val1);
+
+// // null
+// const val2 = null
+// console.log(typeof val2);
+
+
+// STRINGS & STRING METHODS
+
+// const str = 'JavaScript, Programming, is, a, lot, of, fun!'
+
+// // Get the length
+// console.log('THE length of the string is: ', str.length);
+
+// // Change the case
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// // Sub-Strings
+// console.log( str.substr(10) ); // Always starts with 0
+// console.log( str.substring(6, 16) ); // Takes start and the end
+
+// // Split -> split the whole string on the basis of something | Returns an array
+// console.log( str.split(', ') );
